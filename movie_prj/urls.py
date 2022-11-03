0a1c3d9abef0b7fae93044379e5182e2e974bed1
kosclid <kos-2000@hanmail.net>
2022-11-03 17:15:18 +0900

    2022/11/03 마무리

--- movie_prj/urls.py
+++ movie_prj/urls.py
@@ -23,5 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('recommand/', include('recommand.urls')),
+    path('detail/', include('detail.urls')),
 
 ]
